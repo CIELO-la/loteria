991518273e408d952daf7b3ad4518287eb1fd3ca
Farica Mascarenhas <fmacar3na@github.com>
2022-09-02 18:10:37 -0500

    Added win-loss modal
    Added dialog box to notify user if they won or lost.

--- src/App/Juego/FinDelJuego.js
+++ src/App/Juego/FinDelJuego.js
@@ -1,15 +1,64 @@
 import React from "react";
+import Dialog from '@mui/material/Dialog';
+import { createTheme, DialogActions, DialogContent, DialogContentText, DialogTitle, ThemeProvider } from "@mui/material";
+import { Button } from "react-bootstrap";
+import { useHistory } from "react-router-dom";
 
 const FinDelJuego = ({ jugadorId, ganador }) => {
   const isWinner = ganador === jugadorId;
+  const history = useHistory();
+  const dialogTheme = createTheme({
+    components: {
+      MuiDialogTitle: {
+        styleOverrides: {
+          root: {
+            color: '#cb3805',
+            fontFamily: 'fantasy',
+            textAlign: 'center',
+          }
+        }
+      },
+      MuiDialogContentText: {
+        styleOverrides: {
+          root: {
+            textAlign: 'center',
+            fontSize: '80px',
+          }
+        }
+      },
+      MuiDialogActions: {
+        styleOverrides: {
+          root: {
+            margin: '0px',
+            paddingTop: '0px',
+            paddingBottom: '20px',
+            alignContent: 'center',
+            justifyContent: 'center',
+          }
+        }
+      },
+    }
+  })
+
   return(
     <div>
-      {isWinner
-        ? <p>You won!</p>
-        : <p>You lost!</p>
-      }
+      <ThemeProvider theme={dialogTheme}>
+        <Dialog open="true" aria-labelledby="alert-dialog-title" aria-describedby="alert-dialog-description">
+          <DialogTitle id="alert-dialog-title">{isWinner ? ("You won the game!") : ("Better luck next time.") }</DialogTitle>
+          <DialogContent>
+            <DialogContentText id="alert-dialog-description">
+              {isWinner ? 
+              ("\u{1F389}"): ("\u{1F622}")}
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick="">Play Again</Button>
+            <Button onClick={() => history.push("/")}>Home</Button>
+          </DialogActions>
+        </Dialog>
+      </ThemeProvider>
     </div>
-  )
+  );
 };
 
 export default FinDelJuego;
