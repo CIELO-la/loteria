1e2eddd0fe354468badf0a511c1ec00952cdfabc
MacKenzie O <mackenzieo@google.com>
2022-09-07 18:23:16 -0700

    Commit WIP download work for potential handoff

--- src/App/Download/index.js
+++ src/App/Download/index.js
@@ -1,14 +1,16 @@
 import { useTranslation } from "react-i18next";
 import Row from "react-bootstrap/Row";
 import BackButton from "../Common/BackButton";
+import Button from "react-bootstrap/Button";
 
 const Download = ({}) => {
   const { t } = useTranslation();
 
-  // TODO: update the background color on this page
+  // TODO: find a better way to update the background color on this page
+  document.body.style.backgroundColor = "#FEECE9"
 
   return (
-    <div className="">
+    <div className="downloadPage">
       <Row>
         <div className="col-2 salaBackCol">
           <BackButton />
@@ -18,6 +20,35 @@ const Download = ({}) => {
       <Row>
         <div className="prompt justify-content-center">
           {t('downloadPrompt')}
+        </div>
+      </Row>
+      <Row className="mt-4">
+        <div className="downloadSection">
+          <div className="placeholderIcon" />
+          <div className="downloadSectionLabel">
+              Deck of 54 cards
+          </div>
+          <Button className="downloadButton" variant="secondary" size="m">
+            Download
+          </Button>
+        </div>
+      </Row>
+      <Row className="mt-4">
+        <div className="downloadSection">
+          <div className="placeholderIcon" />
+          <div className="downloadSectionLabel">
+              Player boards
+          </div>
+          <input type="number" defaultValue="4" min="1" max="10" className="boardCountInput"
+              onKeyPress={(event) => {
+                if (!/[0-9]/.test(event.key)) {
+                  event.preventDefault();
+                }
+              }}
+            />
+          <Button className="downloadButton" variant="secondary" size="m">
+            Download
+          </Button>
         </div>
       </Row>
     </div>
